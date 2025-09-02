2234a69ca05ebb069d77906d95ae652e5984b7de
Eric Rian <eric.riaan@gmail.com>
2025-09-02 12:34:40 -0300

    comportamento de memoria

---
+++ 4 - Comportamento de memória/primitive.java
@@ -0,0 +1,12 @@
+public class primitive {
+    public static void main(String[] args) {
+        int p1 = 10;
+        int p2 = p1;
+
+        System.out.println(p1);
+        System.out.println(p2);
+        p1 = 20;
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
